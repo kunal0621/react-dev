c55b52c67330063cc354ed9041b52cd0e43c904d
kunal0621 <kunalsinghksr0021@gmail.com>
2024-01-02 16:44:18 +0530

    new PDF framework

---
+++ web-application/src/componentPdf/mainPdf.js
@@ -0,0 +1,73 @@
+import Preview from './PDF/Preview'
+import React, { useState } from 'react'
+function createPDF() {
+  const [profile, setProfile] = useState({
+    type: 'Profile',
+    name: 'John Doe',
+    profession: 'Junior Developer',
+    profileImageURL: 'https://i.imgur.com/f6L6Y57.png',
+    display: true,
+    about: 'About...',
+  })
+
+  const handleChange = (name, value) => {
+    setProfile({ ...profile, [name]: value })
+  }
+
+  return (
+    <div
+      style={{
+        width: '100%',
+        height: '100vh',
+        display: 'flex',
+      }}
+    >
+      <div style={{ width: '50%' }}>
+        <div>
+          <label>Name</label>
+
+          <input
+            name='name'
+            defaultValue={profile.name}
+            onChange={(e) => {
+              handleChange(e.target.name, e.target.value)
+            }}
+          />
+        </div>
+        <div>
+          <label>Profession</label>
+          <input
+            name='profession'
+            defaultValue={profile.profession}
+            onChange={(e) => {
+              handleChange(e.target.name, e.target.value)
+            }}
+          />
+        </div>
+        <div>
+          <label>ImageURL</label>
+          <input
+            name='profileImageURL'
+            defaultValue={profile.profileImageURL}
+            onChange={(e) => {
+              handleChange(e.target.name, e.target.value)
+            }}
+          />
+        </div>
+        <div>
+          <label>About</label>
+          <input
+            name='about'
+            defaultValue={profile.about}
+            onChange={(e) => {
+              handleChange(e.target.name, e.target.value)
+            }}
+          />
+        </div>
+      </div>
+      <Preview profile={profile} />
+    </div>
+  )
+}
+
+export default createPDF
